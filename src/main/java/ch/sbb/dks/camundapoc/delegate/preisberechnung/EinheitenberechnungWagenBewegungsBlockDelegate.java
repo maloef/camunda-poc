6d38dfbf852f519cc6d42587b064607f6dda61db
Loeffler Markus (IT-SWE-CC3-TTF) <markus.loeffler@sbb.ch>
2018-09-13 11:05:22 +0200

    change implementation (wip)

--- src/main/java/ch/sbb/dks/camundapoc/delegate/preisberechnung/EinheitenberechnungWagenBewegungsBlockDelegate.java
+++ src/main/java/ch/sbb/dks/camundapoc/delegate/preisberechnung/EinheitenberechnungWagenBewegungsBlockDelegate.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -35,25 +36,37 @@ public class EinheitenberechnungWagenBewegungsBlockDelegate implements JavaDeleg
         List<WagenEvent> events = (List<WagenEvent>) delegateExecution.getVariable("wagenEvents");
         LOGGER.info("Calculating Einheiten ...");
 
-        List<Wagenbewegungsblock> bloecke = new ArrayList<Wagenbewegungsblock>();
-//        Map<String, List<WagenEvent>> wagennummernToEvents = splitWagenEvents();
+        List<Wagenbewegungsblock> bloecke = new ArrayList<>();
+//        Map<String, List<WagenEvent>> wagennummernToEvents = splitWagenEvents(events);
 //        for (String wagennummer : wagennummernToEvents.keySet()) {
 //            List<WagenEvent> wagenEvents = wagennummernToEvents.get(wagennummer);
+//            bloecke.add(new Wagenbewegungsblock(wagenEvents.get(0), wagenEvents.get(0)));
 //            for (int i = 0; i < wagenEvents.size() - 1; i++) {
 //                bloecke.add(new Wagenbewegungsblock(wagenEvents.get(i), wagenEvents.get(i + 1)));
 //            }
-//            Wagenbewegungsblock wagenbewegungsblock = new Wagenbewegungsblock(wagenEvents.get(wagenEvents.size() - 1), null);
+//            WagenEvent last = wagenEvents.get(wagenEvents.size() - 1);
+//            bloecke.add(new Wagenbewegungsblock(last, last));
 //        }
         bloecke.add(new Wagenbewegungsblock(events.get(0), events.get(1)));
         bloecke.get(0).setWagentyp("Eanos");
         bloecke.add(new Wagenbewegungsblock(events.get(1), events.get(2)));
         bloecke.get(1).setWagentyp("Fas");
         delegateExecution.setVariable("wagenbewegungsbloecke", bloecke);
-
     }
 
-    private Map<String, List<WagenEvent>> splitWagenEvents() {
-        return null;
+    private Map<String, List<WagenEvent>> splitWagenEvents(List<WagenEvent> events) {
+        HashMap<String, List<WagenEvent>> wagennummernToEvents = new HashMap<>();
+        for (WagenEvent event : events) {
+            String nummer = event.getWagennummer();
+            if (wagennummernToEvents.containsKey(nummer)) {
+                wagennummernToEvents.get(nummer).add(event);
+            } else {
+                List<WagenEvent> newEvents = new ArrayList<>();
+                newEvents.add(event);
+                wagennummernToEvents.put(nummer, newEvents);
+            }
+        }
+        return wagennummernToEvents;
     }
 
     private String lookupCluster(String wagennummer) {
